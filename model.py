3a42f225efd589214f43157fbc819a202a5acd30
I.B. Park <the.nulledge@gmail.com>
2018-05-18 12:59:50 +0900

    Adapt stacked hourglass to Human3.6M dataset

--- model.py
+++ model.py
@@ -5,30 +5,28 @@ import module.layers as layers
 
 
 class Model:
-    def __init__(self, stages, features, joints):
-        self.stages = stages
+    def __init__(self, features, joints, z_resolutions):
         self.features = features
         self.joints = joints
+        self.z_resolutions = z_resolutions
 
         with tf.variable_scope('input'):
             self.images = tf.placeholder(
                 name='image',
                 dtype=tf.float32,
                 shape=[None, 256, 256, 3])
-            self.heatmaps_groundtruth = tf.placeholder(
-                name='heatmap_groundtruth',
-                dtype=tf.float32,
-                shape=[None, 64, 64, self.joints])
+            self.voxel_groundtruth = [
+                tf.placeholder(
+                    name='voxel_groundtruth_%d' % stage,
+                    dtype=tf.float32,
+                    shape=[None, 64, 64, self.joints * z_resolution])
+                for stage, z_resolution in enumerate(self.z_resolutions)
+            ]
             self.is_training = tf.placeholder(
                 name='train',
                 dtype=tf.bool,
                 shape=())
-            self.mask = tf.placeholder(
-                name='mask',
-                dtype=tf.float32,
-                shape=[None, self.joints]
-            )
-
+            
         with tf.variable_scope('compress'):
             with tf.variable_scope('conv_bn_relu'):
                 net = layers.conv(inputs=self.images, ksize=7, kchannel=64, kstride=2)  # 128 * 128 * 64
@@ -40,9 +38,8 @@ class Model:
             net = module.bottleneck(inputs=net, kchannel=128, is_training=self.is_training, name='B')  # 64 * 64 * 128
             net = module.bottleneck(inputs=net, kchannel=self.features, is_training=self.is_training, name='C')  # 64 * 64 * 256
 
-        self.heatmaps = []
-        self.outputs = []
-        for stage in range(1, stages + 1):
+        self.voxels = []
+        for stage, z_resolution in enumerate(self.z_resolutions):
             with tf.variable_scope('hourglass_' + str(stage)):
                 prev = tf.identity(net)
                 net = module.hourglass(inputs=net, is_training=self.is_training)  # 64 * 64 * 256
@@ -53,14 +50,13 @@ class Model:
                     net = layers.bn(inputs=net, is_training=self.is_training)
                     net = layers.relu(inputs=net)
 
-                with tf.variable_scope('heatmap'):
-                    heatmap = layers.conv(inputs=net, ksize=1, kchannel=self.joints)  # 64 * 64 * joint
-
-                    self.heatmaps.append(heatmap)
-                    self.outputs.append(layers.merge(self.images, heatmap))
+                with tf.variable_scope('voxel'):
+                    voxel = layers.conv(inputs=net, ksize=1, kchannel=self.joints * z_resolution)  # 64 * 64 * joint*z_resolution
+                    self.voxels.append(voxel)
 
                 net = layers.conv(inputs=net, ksize=1, kchannel=self.features, name='inter') \
-                      + layers.conv(inputs=heatmap, ksize=1, kchannel=self.features, name='heatmap') \
+                      + layers.conv(inputs=voxel, ksize=1, kchannel=self.features, name='voxel') \
                       + prev  # 64 * 64 * 256
-
-        tf.summary.image('result-images', self.outputs[-1])
+            for z in range(z_resolution):
+                tf.summary.image('voxel_%dD_%02d' % (z_resolution, z+1), self.voxels[stage][:, :, z])
+        tf.summary.image('RGB', self.images)
