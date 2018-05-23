f708297085f4c945d07a80c32fa5292376e7c30c
I.B. Park <the.nulledge@gmail.com>
2018-05-23 11:41:13 +0900

    Merge preprocessing

--- H36M/data.py
+++ H36M/data.py
@@ -1,6 +1,7 @@
 import h5py
 import torch.utils.data as torch_data
-from .task import tasks
+from .util import *
+from .task import tasks, Task
 from .annotation import annotations, Annotation
 from os.path import join
 from functools import lru_cache
@@ -8,8 +9,12 @@ from functools import lru_cache
 
 class Data(torch_data.Dataset):
 
-    def __init__(self, annotation_path, image_path, subjects, task):
+    def __init__(self, annotation_path, image_path, subjects, task,
+                 heatmap_xy_coefficient, voxel_xy_resolution, voxel_z_resolutions):
 
+        self.voxel_z_resolutions = voxel_z_resolutions
+        self.voxel_xy_resolution = voxel_xy_resolution
+        self.heatmap_xy_coefficient = heatmap_xy_coefficient
         self.task = task
         self.subjects = subjects
         self.image_path = image_path
@@ -23,20 +28,63 @@ class Data(torch_data.Dataset):
                 self.data[str(task)][str(annotation)] = file[str(annotation)]
 
             image_name_file = join(annotation_path, '%s_images.txt' % task)
-            file = open(image_name_file, 'r')
-            self.data[str(task)][str(Annotation.Image)] = list()
-            for line in file:
-                self.data[str(task)][str(Annotation.Image)].append(line)
+            self.data[str(task)][str(Annotation.Image)] = np.genfromtxt(image_name_file, dtype=str)
 
     @lru_cache(maxsize=1)
     def __len__(self):
         return len(self.data[str(self.task)][str(Annotation.Image)])
 
     def __getitem__(self, index):
-        raw_data = ()
+        raw_data = (self.data[str(self.task)][str(Annotation.Image)][index], )
         for annotation in annotations[str(self.task)]:
             raw_data = raw_data + (self.data[str(self.task)][str(annotation)][index],)
-        return raw_data
+
+        image = None
+        voxels = None
+
+        if self.task == Task.Train:
+            image, voxels = self.preprocess(raw_data)
+
+        return raw_data, image, voxels
 
     def __add__(self, item):
         pass
+
+    def preprocess(self, raw_data):
+        image_name, S, center, part, scale, zind = raw_data
+
+        voxels = list()
+
+        # Extract subject and camera name from an image name.
+        subject, _, camera, _ = decode_image_name(image_name)
+
+        # Pre-calculate constants.
+        image_xy_resolution = 200 * scale
+
+        # Crop RGB image.
+        image = skimage.io.imread('%s/%s/%s' % (self.image_path, subject, image_name))
+        image = crop_image(image, center, scale, 0, 256)
+        # imageio.imwrite('rgb.png', image)
+
+        # Build voxel.
+        voxel_z_fine_resolution = self.voxel_z_resolutions[-1]
+        for voxel_z_coarse_resolution in self.voxel_z_resolutions:
+            # heatmap_z_coefficient is 1, 1, 1, 3, 5, 7, 13 for 1, 2, 4, 8, 16, 32, 64.
+            heatmap_z_coefficient = 2 * math.floor(
+                (6 * self.heatmap_xy_coefficient * voxel_z_coarse_resolution / voxel_z_fine_resolution + 1) / 2) + 1
+
+            # Convert the coordinate from a RGB image to a cropped RGB image.
+            xy = self.voxel_xy_resolution * (part - center) / image_xy_resolution + self.voxel_xy_resolution * 0.5
+
+            voxel = np.ndarray(shape=(self.voxel_xy_resolution, self.voxel_xy_resolution, len(part) * voxel_z_coarse_resolution))
+            for part_idx in range(len(part)):
+                # zind range (1, 64)
+                # z range (0, 63)
+                z = math.ceil(zind[part_idx] * voxel_z_coarse_resolution / voxel_z_fine_resolution) - 1
+                voxel[:, :,
+                part_idx * voxel_z_coarse_resolution: (part_idx + 1) * voxel_z_coarse_resolution] = generate_voxel(
+                    self.voxel_xy_resolution, voxel_z_coarse_resolution,
+                    xy[part_idx], z,
+                    self.heatmap_xy_coefficient, heatmap_z_coefficient)
+            voxels.append(voxel)
+        return image, voxels
--- demo.py
+++ demo.py
@@ -1,6 +1,8 @@
 import json
-from dotmap import DotMap
 import H36M
+import imageio
+import numpy as np
+from dotmap import DotMap
 from H36M.task import Task
 
 with open('config.json') as fd:
@@ -11,7 +13,21 @@ data = H36M.Data(
     image_path=config.image_path,
     subjects=config.subjects,
     task=Task.from_str(config.task),
+    heatmap_xy_coefficient=config.heatmap_xy_coefficient,
+    voxel_xy_resolution=config.voxel_xy_resolution,
+    voxel_z_resolutions=config.voxel_z_resolutions,
 )
 
+
 print(len(data))
-print(data[0])
+raw_data, image, voxels = data[22783]
+print(raw_data)
+imageio.imwrite('tmp/test.png', image)
+voxel_idx = -1
+for z in range(config.voxel_z_resolutions[voxel_idx]):
+    for x in range(config.voxel_xy_resolution):
+        for y in range(config.voxel_xy_resolution):
+            voxels[voxel_idx][y, x, z] = np.max(voxels[voxel_idx][y, x, [config.voxel_z_resolutions[voxel_idx] * part + z for part in range(config.num_parts)]])
+for z in range(config.voxel_z_resolutions[voxel_idx]):
+    imageio.imwrite('tmp/%02d_%02d.png' % (config.voxel_z_resolutions[voxel_idx], z), voxels[voxel_idx][:, :, z])
+    
