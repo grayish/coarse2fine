490eeb2b519cafb2a0e94eb3ee2d20ae1999fc95
grayish <hobbangz5@gmail.com>
2018-06-04 17:36:04 +0900

    missing files

--- demo.py
+++ demo.py
@@ -1,32 +1,49 @@
 import json
-import H36M
+import os
+
+import numpy as np
+import skimage.transform
 import torch
 import torch.nn as nn
-import skimage.transform
-import os
-import numpy as np
-from tqdm import tqdm as tqdm
-from hourglass import StackedHourglass
 from dotmap import DotMap
-from log import log
-from H36M.task import Task
-from torch.utils.data import DataLoader
 from torch.autograd import Variable
+from torch.utils.data import DataLoader
+from tqdm import tqdm as tqdm
 from visdom import Visdom
 
-def merge_to_color_heatmap(batch_heatmaps, h_format='NCHW'):
-    Color = torch.cuda.FloatTensor(
-        [[0, 0, 0.5],
-         [0, 0, 1],
-         [0, 1, 0],
-         [1, 1, 0],
-         [1, 0, 0]]
-    )
+import H36M
+from H36M.task import Task
+from hourglass import StackedHourglass
+from log import log
 
-    if h_format == 'NHWC':
-        batch_heatmaps = batch_heatmaps.permute(0, 3, 1, 2).contiguous()
+Color = torch.FloatTensor(
+    [[0, 0, 0.5],
+     [0, 0, 1],
+     [0, 1, 0],
+     [1, 1, 0],
+     [1, 0, 0]]
+)
 
-    batch, joints, height, width = batch_heatmaps.size()
+
+def merge_to_color_heatmap(batch_heatmaps):
+    batch, joints, depth, height, width = batch_heatmaps.size()
+
+    batch_heatmaps_flat = batch_heatmaps.view(batch, joints, depth, -1)
+    max_depth_idx = batch_heatmaps_flat.max(-1)[0].max(-1)[1]
+
+    test = list()
+    for b_idx in range(batch):
+        test2 = list()
+        for j_idx in range(joints):
+            test2.append(batch_heatmaps[b_idx, j_idx, max_depth_idx[b_idx, j_idx], :, :].view(1, 1, height, width))
+
+        test.append(torch.cat(test2, dim=1))
+
+    batch_heatmaps = torch.cat(test, dim=0)
+    # batch_heatmaps = torch.cat(
+    #     [torch.cat(
+    #         [batch_heatmaps[b_idx, j_idx, max_depth_idx[b_idx, j_idx], :, :] for j_idx in range(joints)], dim=2)
+    #      for b_idx in range(batch)], dim=0)
 
     heatmaps = batch_heatmaps.clamp(0, 1.).view(-1)
 
@@ -41,17 +58,15 @@ def merge_to_color_heatmap(batch_heatmaps, h_format='NCHW'):
 
     color_heatmap = (1.0 - t) * k + t * k_1
     color_heatmap = color_heatmap.view(batch, joints, height, width, 3)
-    color_heatmap = color_heatmap.permute(0, 4, 2, 3, 1) # B3HWC
-    color_heatmap, _ = torch.max(color_heatmap, 4) # B3HW
+    color_heatmap = color_heatmap.permute(0, 4, 2, 3, 1)  # B3HWC
+    color_heatmap, _ = torch.max(color_heatmap, 4)  # B3HW
 
     return color_heatmap
 
 
 def draw_merged_image(heatmaps, images, window):
-    assert viz.check_connection()
-
-    heatmaps = merge_to_color_heatmap(heatmaps.data)
-    heatmaps = heatmaps.permute(0, 2, 3, 1).cpu()  # NHWC
+    heatmaps = merge_to_color_heatmap(heatmaps)
+    heatmaps = heatmaps.permute(0, 2, 3, 1)  # NHWC
 
     resized_heatmaps = list()
     for idx, ht in enumerate(heatmaps):
@@ -66,6 +81,7 @@ def draw_merged_image(heatmaps, images, window):
     overlayed_image = overlayed_image.transpose(0, 3, 1, 2)
 
     return viz.images(tensor=overlayed_image, nrow=3, win=window)
+
 
 viz = Visdom()
 
@@ -96,7 +112,7 @@ if __name__ == "__main__":
             voxel_xy_resolution=config.voxel_xy_resolution,
             voxel_z_resolutions=config.voxel_z_resolutions,
         ),
-        config.batch, shuffle=True, pin_memory=True,
+        config.batch, shuffle=True, pin_memory=False,
         num_workers=config.workers,
     )
     log.info('Done')
@@ -142,34 +158,35 @@ if __name__ == "__main__":
         with tqdm(total=len(loader), unit=' iter', unit_scale=False) as progress:
             progress.set_description('Epoch %d' % epoch)
 
-            for raw_data, image, voxels in loader:
-
-                images = to_var(image).to(torch.float)
-                for idx, voxel in enumerate(voxels):
-                    voxels[idx] = to_var(voxel).to(torch.float)
-
-                optimizer.zero_grad()
-                outputs = model(images)
-
-                loss = sum([criterion(out, voxel) for out, voxel in zip(outputs, voxels)])
-                loss.backward()
-
-                optimizer.step()
-
-                progress.set_postfix(loss=float(loss.data))
-                progress.update(1)
-
-                step = step + config.batch
-
-                assert viz.check_connection()
-                loss_window =  viz.line(X=step,
-                    Y=np.array([float(loss.data)]),
-                    win=loss_window,
-                    update='append' if loss_window is not None else None)
-
-                out = outputs[-1].squeeze().contiguous()
-                images = images.cpu().numpy()
-                out_image_window = draw_merged_image(voxels[-1], images.copy(), out_image_window)
+            xx = torch.ones(5)
+            for image in loader:
+                xx = xx + 1
+                # images = to_var(image).to(torch.float)
+                # for idx, voxel in enumerate(voxels):
+                #     voxels[idx] = to_var(voxel).to(torch.float)
+                #
+                # optimizer.zero_grad()
+                # outputs = model(images)
+                #
+                # loss = sum([criterion(out, voxel) for out, voxel in zip(outputs, voxels)])
+                # loss.backward()
+                #
+                # optimizer.step()
+                #
+                # progress.set_postfix(loss=float(loss.data))
+                # progress.update(1)
+                #
+                # step = step + config.batch
+                #
+                # assert viz.check_connection()
+                # loss_window = viz.line(X=step,
+                #                        Y=np.array([float(loss.data)]),
+                #                        win=loss_window,
+                #                        update='append' if loss_window is not None else None)
+                #
+                # out = outputs[-1].squeeze().contiguous()
+                # viz.images(image.numpy(), win='image')
+                # out_image_window = draw_merged_image(voxels[-1], images.copy(), out_image_window)
                 gt_image_window = draw_merged_image(out, images.copy(), gt_image_window)
 
         log.info('Saving the trained model... (%d epoch, %d step)' % (epoch, step))
